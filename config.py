4e1950b44ed2c2082c5d978c5c79ff8e2c01da0e
Purvee_Parikh <purvee.parikh@adani.com>
2025-05-31 22:09:09 +0530

    delete the unstructured files

--- config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv
-
-VC_API_KEY = os.getenv('VC_API_KEY')
-VC_API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
-
